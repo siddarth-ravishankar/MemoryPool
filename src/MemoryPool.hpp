e886516a547bd66f3c696223bc056eb674b947f2
Siddarth Ravishankar <siddarth008@gmail.com>
2019-06-09 17:39:12 -0700

    Lock when obtaining available segments.

--- src/MemoryPool.hpp
+++ src/MemoryPool.hpp
@@ -20,15 +20,19 @@ class MemoryPool {
 	size_t segment_size;
 	int pool_size;
 	void* head;
+	ELEM_TYPE* last_segment;
 	ELEM_TYPE* tail_segment;
 	std::forward_list<ELEM_TYPE*> available_segments;
+	std::mutex segment_mutex;
 	
+	ELEM_TYPE* getTailSegment();
 	ELEM_TYPE* getNextAvailableSegment();
 	
 public:
 	MemoryPool();
 	explicit MemoryPool(const int);
 	~MemoryPool();
+	
 	template <class... Args> ELEM_TYPE* alloc(Args&&...);
 	void free(ELEM_TYPE*);
 };
@@ -38,10 +42,12 @@ MemoryPool<ELEM_TYPE>::MemoryPool() : MemoryPool::MemoryPool(DEFAULT_POOL_SIZE) 
 
 template <class ELEM_TYPE>
 MemoryPool<ELEM_TYPE>::MemoryPool(const int _pool_size) {
-	segment_size = sizeof(ELEM_TYPE);
+	
 	pool_size = _pool_size;
 	head = std::malloc(pool_size);
+	segment_size = sizeof(ELEM_TYPE);
 	tail_segment = reinterpret_cast<ELEM_TYPE*>(head);
+	last_segment = reinterpret_cast<ELEM_TYPE*>(head) + pool_size - segment_size;
 }
 
 template <class ELEM_TYPE>
@@ -50,17 +56,30 @@ MemoryPool<ELEM_TYPE>::~MemoryPool() {
 }
 
 template <class ELEM_TYPE>
+ELEM_TYPE* MemoryPool<ELEM_TYPE>::getTailSegment() {
+	
+	if (tail_segment > last_segment)
+		throw "Unable to allocate memory. Memory pool depleated.";
+	
+	ELEM_TYPE* next_segment = tail_segment;
+	tail_segment++;
+	return next_segment;
+}
+
+template <class ELEM_TYPE>
 ELEM_TYPE* MemoryPool<ELEM_TYPE>::getNextAvailableSegment() {
 	
 	ELEM_TYPE* next_available_segmennt;
 
-	if(available_segments.empty()) {
-		next_available_segmennt = tail_segment;
-		tail_segment++;
-	}
-	else {
-		next_available_segmennt = available_segments.front();
-		available_segments.pop_front();
+	std::lock_guard<std::mutex> lock(segment_mutex);
+	{
+		if(available_segments.empty()) {
+			next_available_segmennt = getTailSegment();
+		}
+		else {
+			next_available_segmennt = available_segments.front();
+			available_segments.pop_front();
+		}
 	}
 	
 	return next_available_segmennt;
@@ -69,12 +88,16 @@ ELEM_TYPE* MemoryPool<ELEM_TYPE>::getNextAvailableSegment() {
 template <class ELEM_TYPE>
 template <class... Args>
 ELEM_TYPE* MemoryPool<ELEM_TYPE>::alloc(Args&&... args) {
+	
 	ELEM_TYPE* ptr = getNextAvailableSegment();
+	
 	return new(ptr) ELEM_TYPE(std::forward<Args>(args)...);
 }
 
 template <class ELEM_TYPE>
 void MemoryPool<ELEM_TYPE>::free(ELEM_TYPE* ptr) {
+	
+	std::lock_guard<std::mutex> lock(segment_mutex);
 	available_segments.push_front(ptr);
 }
 
